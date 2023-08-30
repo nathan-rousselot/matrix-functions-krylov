474b40782eae17e7d6413fe2e6b834eabe9eb0a9
Nathan ROUSSELOT <nathan.rousselot@exail.com>
2023-08-30 20:32:48 +0200

    Shifted-Invert Code + ToC

--- src/shifted_arnoldi_convergence.m
+++ src/shifted_arnoldi_convergence.m
@@ -1,16 +1,16 @@
-function A_est = shifted_arnoldi_convergence(A,K)
+function shifted_arnoldi_convergence(A,K)
     err = zeros(K,1);
     n = length(A);
     u = randn(n,1);
     u = u/norm(u,2);
     true_lambda = eigs(A);
-    [~,H] = arnoldi_iteration(A,u,K);
-    sigma = max(eigs(H));
-    for k = length(true_lambda):K
-        [V,H,sigma] = shifted_arnoldi_iteration(A,u,k,sigma);
-        lambda_shifted = eigs(H);
-        lambda = sigma+1./lambda_shifted;
-        err(k) = norm(lambda-true_lambda,2)/norm(true_lambda,2);
+    sigma = (true_lambda(1)+true_lambda(2))/2;
+    %sigma = min(true_lambda);
+    for k = 1:K
+        [~,H,sigma] = shifted_arnoldi_iteration(A,u,k,sigma);
+        mu = eigs(H);
+        lambda = sigma+1./mu;
+        err(k) = norm(lambda-true_lambda(1:length(lambda)),2)/norm(true_lambda(1:length(lambda)),2);
         disp(k)
     end
     close all
@@ -19,7 +19,7 @@ function A_est = shifted_arnoldi_convergence(A,K)
     xlabel('Dimension of the Krylov space')
     ylabel('Relative Error')
     grid on
-    % matlab2tikz('sign_shift_bad.tex')
+    % matlab2tikz('sign_shift_good.tex')
 
     figure(2)
     plot(complex(true_lambda),'ko')
@@ -28,5 +28,5 @@ function A_est = shifted_arnoldi_convergence(A,K)
     xlabel('Real')
     ylabel('Imaginary')
     legend('Eigenvalues','Ritz Values')
-    % matlab2tikz('ritz_shift_bad.tex')
+    % matlab2tikz('ritz_shift_good.tex')
 end
--- src/shifted_arnoldi_iteration.m
+++ src/shifted_arnoldi_iteration.m
@@ -1,9 +1,14 @@
 function [V,H,sigma] = shifted_arnoldi_iteration(A,v1,k,sigma)
     n = length(v1);
+    % tic
+    % [~,non_shifted] = arnoldi_iteration(A,v1,length(n/5));
+    % sigma = median(eigs(non_shifted));
+    % disp('Time to compute sigma:')
+    % toc
     [V,H]= deal(zeros(n,n));
     V(:,1) = v1;
+    [L, U] = lu(A - sigma * eye(n));
     for i = 1:k
-        [L,U] = lu(A-sigma*eye(n));
         w = U\(L\V(:,i));
         for j = 1:i
             H(j,i) = V(:,j)'*w;
@@ -16,8 +21,6 @@ function [V,H,sigma] = shifted_arnoldi_iteration(A,v1,k,sigma)
             return;
         end
         V(:,i+1) = w/H(i+1,i);
-        H_temp = H(1:k,1:k);
-        sigma = max(eigs(H_temp));
     end
     H = H(1:k,1:k);
     V = V(1:n,1:k);
