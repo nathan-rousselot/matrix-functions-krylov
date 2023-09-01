ff58337c98e56a19cfb55419ff7672db142d9cc7
Nathan ROUSSELOT <nathan.rousselot@exail.com>
2023-09-01 11:05:27 +0200

    Finished kp estimator + documentation

--- src/compute_kp.m
+++ src/compute_kp.m
@@ -1,12 +1,15 @@
-function kp = compute_kp(A,k,N)
+function kp = compute_kp(A,k,N,lb,ub)
     q = zeros(N,1);
     n = length(A);
     for i = 1:N
         u = randn(n,1);
+        sigma = lb+(ub-lb)*rand(1,1);
         u = u/norm(u,2);
         [~,H] = arnoldi_iteration(A,u,k);
-        q(i) = trace((sign(H)));
+
+        % [~,H,~] = shifted_arnoldi_iteration(A,u,k,sigma);
+        q(i) = trace(sign(H));
     end
     kp = mode(q);
-    kp = (kp+k)/2;
+    kp = real((kp+k)/2);
 end
---
+++ src/demo_compute_kp.m
@@ -0,0 +1,27 @@
+function demo_compute_kp(A,N,k)
+    if nargin < 3
+        k = 20;
+    end
+    if nargin < 2
+        N=100;
+    end
+    % rng(42);
+    lambda = eig(full(A));
+    % sigma = (lambda(1)+lambda(2))/2;
+    kp_exact = length(lambda(lambda>0));
+    lb = min(lambda);
+    ub = max(lambda);
+    kp = zeros(N,1);
+    for j = 1:N
+        kp(j) = compute_kp(A,k,j,lb,ub);
+        disp(j)
+    end
+    figure;
+    plot(kp,'k')
+    hold on
+    plot([1 N], [4 4],'k--')
+    xlabel('Number of Sampling')
+    ylabel('Number of Positive Eigenvalues')
+    legend('Estimation','True Value')
+
+end
